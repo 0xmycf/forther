746421d9a56d9b742b818e60768a1dccb42508a7
0xmycf <mycf.mycf.mycf@gmail.com>
2024-06-14 15:32:35 +0200

    haddock for stack (with typos)

--- src/Stack.hs
+++ src/Stack.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE PatternSynonyms, ViewPatterns #-}
 module Stack
  ( Stack(..)
+ -- * Patterns for matching
+ , pattern Empty
+ , pattern Head
+ , pattern Tail
+ , pattern Cons
+ -- * Functinos
+ -- ** Accessorr
  , unstack
+ -- ** Constructors
  , empty
  , singleton
+ -- ** Stack Operations
  , push
  , pop
  , popN
@@ -11,24 +21,50 @@ module Stack
 
 newtype Stack a
   = Stack [a]
+  deriving Show
 
+pattern Empty :: Stack a
+pattern Empty = Stack []
+
+pattern Head :: a -> Stack a
+pattern Head x <- (unstack -> x : _)
+
+pattern Tail :: [a] -> Stack a
+pattern Tail xs <- (unstack -> _ : xs)
+
+pattern Cons :: a -> [a] -> Stack a
+pattern Cons x xs <- (unstack -> x : xs)
+  where Cons x xs = Stack (x : xs)
+
+-- | unwraps the underlying list
+-- >>> unstack (Stack [1,2,3])
+-- [1,2,3]
 unstack :: Stack a -> [a]
 unstack (Stack ls) = ls
 
+-- |
+-- >>> empty
+-- Stack []
 empty :: Stack a
 empty = Stack []
 
+-- |
+-- >>> singleton 1
+-- Stack [1]
 singleton :: a -> Stack a
 singleton = Stack . (:[])
 
 push :: a -> Stack a -> Stack a
 push a (Stack ls) = Stack (a : ls)
 
+-- |
+-- >>> pop (Stack [1,2,3])
+-- Just (1,Stack [2,3])
 pop :: Stack a -> Maybe (a, Stack a)
 pop (Stack (a : as)) = Just (a, Stack as)
 pop _                = Nothing
 
--- TODO still unsafe
+-- | TODO still unsafe
 popN :: Int -> Stack a -> Maybe ([a], Stack a)
 popN = go []
   where
